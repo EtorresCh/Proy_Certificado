9338a4c9a6c8959ba0658edb6bb72957457bedae
EtorresCh <edgarjtorreschafloque3@gmail.com>
2025-04-17 18:39:17 -0500

    Primera Version

---
+++ view/UsuPerfil/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php require_once '../html/MainHead.php';?>
+    <title>Home</title>
+  </head>
+  <body>
+     <?php require_once '../html/MainMenu.php';?>
+     <?php require_once '../html/MainHeader.php';?>  
+    <div class="br-sideright">
+      <ul class="nav nav-tabs sidebar-tabs" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" data-toggle="tab" role="tab" href="#contacts"><i class="icon ion-ios-contact-outline tx-24"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" role="tab" href="#attachments"><i class="icon ion-ios-folder-outline tx-22"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" role="tab" href="#calendar"><i class="icon ion-ios-calendar-outline tx-24"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" role="tab" href="#settings"><i class="icon ion-ios-gear-outline tx-24"></i></a>
+        </li>
+      </ul><!-- sidebar-tabs -->
+    </div>
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="index.html">Perfil</a>
+        </nav>
+      </div>
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Perfil</h4>
+        <p class="mg-b-0">Pantalla perfil</p>
+      </div>
+      <div class="br-pagebody">
+      </div>
+    </div>
+    <?php require_once '../html/MainJs.php';?>
+  </body>
+</html>
